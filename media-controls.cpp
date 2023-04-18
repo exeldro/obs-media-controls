1cf72b7d08eb40c7da4efb668c78da1abd09e76c
Exeldro <exelsor@hotmail.com>
2023-04-18 09:29:27 +0200

    fix delete control timing

--- media-controls.cpp
+++ media-controls.cpp
@@ -282,12 +282,12 @@ void MediaControls::RefreshMediaControls()
 			obs_get_source_by_name(QT_TO_UTF8(w->objectName()));
 		if (!source) {
 			ui->verticalLayout->removeItem(item);
-			delete w;
+			w->deleteLater();
 		} else {
 			if (obs_source_removed(source) ||
 			    (!allSources && !obs_source_active(source))) {
 				ui->verticalLayout->removeItem(item);
-				delete w;
+				w->deleteLater();
 			}
 			obs_source_release(source);
 		}
